a0ce211a38ad8a0c3b562294a90d65727dc37e14
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-02-06 00:41:51 +0000

    Add job application styling and HTML logic for layout + edit functionality to tasks

--- web_app/app.py
+++ web_app/app.py
@@ -2,6 +2,7 @@
 from config import SECRET_KEY
 from scripts.build_calendar import CalendarBuild
 from scripts.build_todo import TodoBuild
+from scripts.build_job_applications import JobApplicationBuild
 from flask import Flask, render_template, url_for, redirect, session, request
 from datetime import datetime
 
@@ -17,7 +18,7 @@ def home():
         session['year'] = current_time.year
 
     if 'month_display' not in session:
-        session['month-display'] = True
+        session['month_display'] = True
 
     month = session['month']
     year = session['year']
@@ -34,6 +35,26 @@ def home():
 
     todo_list = TodoBuild.todo_list
     todo_complete = TodoBuild.todo_complete
+    job_list = JobApplicationBuild.job_list
+
+    if 'collapse_todo' not in session:
+        session['collapse_todo'] = False
+
+    if 'collapse_todo_complete' not in session:
+        session['collapse_todo_complete'] = False
+
+    if 'edit_task' not in session:
+        session['edit_task'] = False
+
+    collapse_todo_list = session['collapse_todo']
+    collapse_complete = session['collapse_todo_complete']
+
+    task_edit = session['edit_task']
+
+    if 'collapse_job_applications' not in session:
+        session['collapse_job_applications'] = False
+
+    collapse_job = session['collapse_job_applications']
 
     return render_template(
         'index.html',
@@ -47,11 +68,16 @@ def home():
         num_of_weeks=num_of_weeks,
         year_layout=year_layout,
         todo_list=todo_list,
-        todo_complete=todo_complete
+        todo_complete=todo_complete,
+        collapse_todo=collapse_todo_list,
+        collapse_complete=collapse_complete,
+        edit_task=task_edit,
+        job_list=job_list,
+        collapse_job=collapse_job
     )
 
-@app.route('/prev_month', methods=['POST'])
-def prev():
+@app.route('/prev_cal', methods=['POST'])
+def prev_cal():
     month = session['month']
     year = session['year']
 
@@ -68,8 +94,8 @@ def prev():
 
     return redirect(url_for('home'))
 
-@app.route('/next_month', methods=['POST'])
-def next():
+@app.route('/next_cal', methods=['POST'])
+def next_cal():
     month = session['month']
     year = session['year']
 
@@ -118,6 +144,53 @@ def complete_task():
 def delete_complete_task():
     task = request.form.get('task', '')
     TodoBuild.delete_task_complete(task)
+    return redirect(url_for('home'))
+
+@app.route('/collapse_todo', methods=['POST'])
+def collapse_todo():
+    if session['collapse_todo']:
+        session['collapse_todo'] = False
+    else:
+        session['collapse_todo'] = True
+    return redirect(url_for('home'))
+
+@app.route('/collapse_todo_complete', methods=['POST'])
+def collapse_todo_complete():
+    if session['collapse_todo_complete']:
+        session['collapse_todo_complete'] = False
+    else:
+        session['collapse_todo_complete'] = True
+    return redirect(url_for('home'))
+
+@app.route('/edit_task', methods=['POST'])
+def edit_task():
+    if session['edit_task']:
+        session['edit_task'] = False
+    else:
+        session['edit_task'] = True
+
+    task_id = request.form.get('task_id', '')
+    new_value = request.form.get('new_task', '')
+    if task_id and new_value:
+        TodoBuild.edit_task(task_id, new_value)
+    return redirect(url_for('home'))
+
+@app.route('/new_job', methods=['POST'])
+def new_job():
+    company = request.form.get('company', '')
+    role = request.form.get('role', '')
+    link = request.form.get('link', '')
+    location = request.form.get('location', '')
+    date = request.form.get('date', '')
+    JobApplicationBuild(company, role, link, location, date)
+    return redirect(url_for('home'))
+
+@app.route('/collapse_job_applications', methods=['POST'])
+def collapse_job_applications():
+    if session['collapse_job_applications']:
+        session['collapse_job_applications'] = False
+    else:
+        session['collapse_job_applications'] = True
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
