5e95f390e5e6b9a4a571ad1adca1eaf65eed5ad9
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-02-06 14:54:05 +0000

    Add delete and status code

--- scripts/build_job_applications.py
+++ scripts/build_job_applications.py
@@ -1,6 +1,7 @@
 # ./scripts/job_application_trackers.py
 import uuid
 
+
 class JobApplicationBuild:
     job_list = {}
 
@@ -16,5 +17,30 @@ class JobApplicationBuild:
             'role': self.role,
             'link': self.link,
             'location': self.location,
-            'date': self.date
+            'date': self.date,
+            'status': None
         }
+
+    @classmethod
+    def delete_job(cls, job_id):
+        try:
+            job_uuid = uuid.UUID(job_id)
+        except ValueError:
+            print('Invalid job_id format:', job_id)
+            return
+        if job_uuid in cls.job_list:
+            del cls.job_list[job_uuid]
+        else:
+            print('Job id not found', job_uuid)
+
+    @classmethod
+    def add_status(cls, job_id, status):
+        try:
+            job_uuid = uuid.UUID(job_id)
+        except ValueError:
+            print('Invalid job_id format:', job_id)
+            return
+        if job_uuid in cls.job_list:
+            cls.job_list[job_uuid]['status'] = status
+        else:
+            print('Job id not found', job_uuid)
